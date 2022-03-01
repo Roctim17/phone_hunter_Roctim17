d4669496a19edfef32b4fbccbeb1a7e2bb51c0b2
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-03-01 22:25:12 +0600

    work

--- app.js
+++ app.js
@@ -17,14 +17,27 @@ const displayPhone = data => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
-                    <img src="${singleData.image}" class="card-img-top" alt="...">
+        <div class="card m-auto" style="width: 18rem;">
+                    <img src="${singleData.image}" class="card-img-top" alt="..."  style="width=80%" height="70%">
                     <div class="card-body">
-                        <h5 class="card-title">${singleData.phone_name}</h5>
-                        <p class="card-text">${singleData.slug}</p>
+                        <h5 class="card-title text-center">${singleData.phone_name}</h5>
+                        <p class="card-text text-center">${singleData.slug}</p>
+                        <a href="#"  onclick="LoadphoneDetails()" class="btn btn-primary text-center">Details</a>
                     </div>
                 </div>
         `;
         searchPhoneResult.appendChild(div);
     })
 }
+
+const LoadphoneDetails = data => {
+
+}
+
+// const LoadphoneDetails = () => {
+
+//     const url = `https://openapi.programming-hero.com/api/phone/${singleData.slug}`;
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+// }
